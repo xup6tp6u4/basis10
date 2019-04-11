bd4c3776673a3e11159fe188a2cab1c35ea47a18
xup6tp6u4 <48340287+xup6tp6u4@users.noreply.github.com>
2019-04-11 17:29:18 +0800

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>     
+#include<stdlib.h>
+int main(void){
+	char name[100];   
+	int i=0;         
+	while(true){
+		printf("¿é¤J¦r¦ê\n");
+		scanf("%s",&name);  
+		while(i<100&&name[i]>=65){                
+			if(name[i]>=65&&name[i]<=90){  
+				name[i]+=32;
+				printf("%c",name[i]);
+			}
+			else if(name[i]>=97&&name[i]<=122){  
+				name[i]-=32;
+				printf("%c",name[i]);
+			}
+			i++;
+		}
+		printf("\n");
+		i=0;
+	} 
+	
+	
+} 
